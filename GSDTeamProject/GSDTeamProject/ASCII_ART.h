ddc3368963c19437ff90b7140c54b53348c3ad31
Hognshi <123186080+Hognshi@users.noreply.github.com>
2023-12-10 22:54:43 +0900

    시작화면

---
+++ GSDTeamProject/GSDTeamProject/ASCII_ART.h
@@ -0,0 +1,97 @@
+#pragma once
+#include<iostream>
+
+void startScene()
+{
+	printf("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■\n");
+	printf("                                                                                                                                            \n");
+	printf("                                                                                                                                            \n");
+	printf("                                                                                                                                            \n");
+	printf("                                                                                                                                            \n");
+	printf("                                                      KRQBBBQBBBQBBBBBBBBBBBBBQBgr                                                          \n");
+	printf("                                                      BQ15PKPKPKqXX2S5qKPKPKPSSZBB                                                          \n");
+	printf("                                                rBE   BB                        QB   .BB.    rr                                             \n");
+	printf("                                         BBB    vBB   BB          :BB          .BB    XI    iBBv                                            \n");
+	printf("                                 :BQ     rDL          QB          BBBI         .QB           ::     vBB                                     \n");
+	printf("                                 7BB                  BQ          BBBY         .BQ                  :BD                                     \n");
+	printf("                                                      BB    PBBBBBBBQBBBBBBB   .BQ                          ..                              \n");
+	printf("                          BBB                         BB    BBBBBBBBBBBQBBBB.  .BD                         .BB7                             \n");
+	printf("                          vBs                         BB          BBBu         :QZ                          ir                              \n");
+	printf("                                                      BB          BQBY         :BK                                ::                        \n");
+	printf("                    7BM           :BBBBBQBBBQBBBBBQBBBBB          BBBd         rBBBBBBBBQBBBQBBBBBQBBBBv          BB                        \n");
+	printf("                     r.           bBBL1uUu1uU1UuU1Uj1Y7i          2BB:          v7ujU111U1U1UuUu2u1uJPBB                                    \n");
+	printf("                                  2Bi                                                                 BB                                    \n");
+	printf("                Sv                                                                                                    .BR                   \n");
+	printf("               .BB                   .r               ..                                                              .BS                   \n");
+	printf("                       sdRBBBBMD.    BBu           5BBBBBBBY           1B       .B     .5        :bBBBBBBBBBBM:                             \n");
+	printf("                      :BQBBBQBQBD    EBB         :BBBr.::uBBB          BB:      2Bs    BB7      BBQBBBRRRBBBBBr                             \n");
+	printf("                              BBM :::ZBB         BBd      iBB          BBi   .. QBM   .BB5     rBB                                          \n");
+	printf("             QB.              BBBBQBBBQB.        gBBRurrLRBBR          BBBQBBBBBQBBBQBBBBq     vQB                       BBB                \n");
+	printf("             QB:              BBB     BBJ         .PBBBBBBP.           BBJ      gBBuuvLBBS     rBBBQBBQEQBBBBBB          rQv                \n");
+	printf("                              QBB     QBB                              BBP  ....QBBBBBBBBS      rJgBBBBBQgggDMK                             \n");
+	printf("                              :BQ      2r    iqPgQBBBBBBBBBQBQBBBS     YBBBBBBBBBB7    BBP            .BB                  vi               \n");
+	printf("           iBB                               dBBQBBBQg5uvriiirrL7                      BBi     iii::..SBB..:ivIu          5BB               \n");
+	printf("            Lr             :jbQBBQBBBBBBBr       :                            .LZBBBPi         BBBBBBBBQBBBBBBBB                            \n");
+	printf("                     r    PBBBBBBQEK1J1BBu      BB                         :BBBBBBBBBBQQ            .rsI1dPr                                \n");
+	printf("                   KBB    BB7          BB7      BBr                       YBBq.       BBB         5BBBBBBBBBBv          .                   \n");
+	printf("                  BBBBB   BBI          BBS      BB7                       QBB         .BBi       BBB        BBv       .BB:..                \n");
+	printf("            .PBBBBBBBBBB  BBB   .....  QBB      BBB:ir77vvsLY7vvj.        iBBP      .5BBB        QBR        BBP      5BBBBBBBBY             \n");
+	printf("           QQBBBBBBBQBBBB  QBBQBBBBBQBBBBD      iBBBBBBBBBBBBBBBBD         :BBBBBBBBBBB7          BBBBRbqQBBBQ     :QBBBBBQBQBBB:           \n");
+	printf("          BBBBBBBBBBBBBBBQJ    ...:.::..                                      ijbbqL:               uBBBBQBv     iBBBQBBBBBQBBBBB.          \n");
+	printf("         1BBBBBBBBBBQBBBQBQBX.                                                                                .KQBBBQBBBBBBBQBBBBB          \n");
+	printf("         XBQBBBBBQBBBBBBBBBBBBBBDJi.                                                                      :vgBBQBBBBBBBBBBBBBQBQBB          \n");
+	printf("          BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB                :B            7B                    .:iUPZQBBBBBBBBBQBBBBBBBQBBBBBBBB:          \n");
+	printf("          .BBBBBBBBBBBBBBBBBBBBBBQBBBBBBBBBB                JB            gB                   QBBBBBBBBBBBBBBBBBBBBBQBQBQBQBBBBi           \n");
+	printf("            7BBBBBBBBBBBBBBBQBQBBBBBBBBBBBBBBBQBBBQBBBBBBBQBBBBBBBBBQBBBBBBBBBBBBBBBQBBBBBQBBBQBBBBBBBQBBBBBBBBBBBQBBBBBQBBBBB7             \n");
+	printf("                .rKQBQBBBBBBBBBBBBBBBQBbRX.                                                      rqjgQBBBBBBBBBBBBBBBBBE1:                  \n");
+	printf("                                                                                                                                        \n");
+	printf("                                                                                                                                            \n");
+}
+
+void WindowBed()
+{
+	printf("                                                                                                                                            \n");
+	printf("                                                                                                                                            \n");
+	printf("                                                                                                                                            \n");
+	printf("                                                                                                                                            \n");
+	printf("                           [창문]                                                                                                           \n");
+	printf("                                                                                                                                            \n");
+	printf("           .BRj152S2525I5U2LBBZsIU52525IS2IUjQB.                                                                                            \n");
+	printf("           .Bi              dB.              7B.                                                                                            \n");
+	printf("           .B7              QBi              JB.                                                                                            \n");
+	printf("           .B7              gB:              JB.                                                                                            \n");
+	printf("           .Br              DB:              vB.                                                                                            \n");
+	printf("           .BB              BBB              BB.                                                                                            \n");
+	printf("           .BBBBBQBQBBBQBBBBBBBBBBBBBBBQBBBBBBB.                                                                                            \n");
+	printf("           .Bgvuj1j1j1u1uuY7QBqvJujUjUu1j1JJ7QQ.                                                                                            \n");
+	printf("           .Bi              EB.              rB.                                                                                            \n");
+	printf("           .BL              QBi              jB:                                                                                            \n");
+	printf("           .Br              MB:              vB.                                                                                            \n");
+	printf("           .BY              DBi              UB.                                                                                            \n");
+	printf("           .BB              BBB              BB.                                                                                            \n");
+	printf("           :BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB:                                                                                            \n");
+	printf("                                                                                                                                            \n");
+	printf("                                                                          [침대]                                                            \n");
+	printf("                                           sI2SSXXKX57                                                                                      \n");
+	printf("                                           B         B                                                                                      \n");
+	printf("                                           B.        B .qbZEDEDdZZZZDEZdDEDEZEZdZEZEDEZEZEDEZdDdZEDEDEZdEqi                                 \n");
+	printf("                                           BBBBBQBBBBB BB............................................... UB                                 \n");
+	printf("                                     QBbgEPIPPEbEbEPK11BB .............................................  BQ                                 \n");
+	printf("                                     B:                DQ .::::::::::::::::::::::::::::::::::::::::::::. BR                                 \n");
+	printf("                                     Bi                RB :::::::::::::::::::::::::::::::::::::::::::::. BQ                                 \n");
+	printf("                                     Bi                BB .::::::::::::::::::::::::::::::::::::::::::::: BQ                                 \n");
+	printf("                                     Br                QB :::::::::::::::::::::::::::::::::::::::::::::..QQ                                 \n");
+	printf("                                     Bi                MQ..::::::::::::::::::::::::::::::::::::::::::::: BR                                 \n");
+	printf("                                     Br               .QB :::::::::::::::::::::::::::::::::::::::::::::..QQ                                 \n");
+	printf("                                     Br   QBbggMgMgMgD5BB .::::::::::::::::::::::::::::::::::::::::::::: BR                                 \n");
+	printf("                                     B7   B:           ZB :::::::::::::::::::::::::::::::::::::::::::::..BQ                                 \n");
+	printf("                                     B7   Qr           QB..::::::::::::::::::::::::::::::::::::::::::::: BR                                 \n");
+	printf("                                     B7   Br           QB .::::::::::::::::::::::::::::::::::::::::::::. QQ                                 \n");
+	printf("                                     Br   Br           BB .............................................. BR                                 \n");
+	printf("                                     B7   Br           BBi77v7v7v7v7v7v7v7v7v7v7v7v7v7v7v7v7v7v77:i77ri:iBB                                 \n");
+	printf("                                     Br   Br           sBBBBBBBBQBBBBBBBBBBBBBBBBBQBBBBBBBBBBBBBQBBQQQBBRB7                                 \n");
+	printf("                                     Qi   Br                                                     MP   ED                                    \n");
+	printf("                                     BD   Qv                                                     BB   BB                                    \n");
+	printf("                                     rXXPXS                                                      B5XqXSB                                    \n");
+	printf("                                                                                                                                            \n");
+}
---
+++ GSDTeamProject/GSDTeamProject/GSDTeamProject.cpp
@@ -0,0 +1,89 @@
+﻿#pragma once
+#include <iostream>
+#include "ASCII_ART.h"
+
+using namespace std;
+
+class item
+{
+private:
+    int peg = 0; int wire = 1; int stick = 0; int nipper = 1; int USB = 0; int data = 0;
+public:
+    string itemList[6];
+    item()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            itemList[i] = " ";
+        }
+        if (nipper == 1)
+        {
+            itemList[0] = "니퍼 ";
+        }
+        if (stick == 1)
+        {
+            itemList[1] = "낡은 나무 막대기 ";
+        }
+        if (wire == 1)
+        {
+            itemList[2] = "철사 ";
+        }
+        if (peg == 1)
+        {
+            itemList[3] = "녹슨 못 ";
+        }
+        if (USB == 1)
+        {
+            itemList[4] = "USB ";
+        }
+        if (data == 1)
+        {
+            itemList[4] = "자료 ";
+        }
+    }
+
+};
+
+class room
+{
+private:
+    int next = 0; int check_space = 0; int bed_under = 0; int pot_break = 0; int picture_break = 0; int lock1 = 0;
+    int isolation_open1 = 0; int isolation_open2 = 0; int isolation_end = 0; int chest_3_judge = 0; int isolation_out = 0; int isolation_o;
+public:
+
+};
+
+int main()
+{
+    system("mode con cols=150 lines=50");
+    int end = 0;
+    item item;
+    startScene();
+
+    while (end != 1)
+    {
+        for (int i = 0; i < 70; i++)
+        {
+            cout << "■";
+        }
+        cout << "\n\n\n";
+        for (int i = 0; i < 70; i++)
+        {
+            cout << "■";
+        }
+        cout << "\n";
+
+        string input;
+        cout << "item list : ";
+        for (int i = 0; i < 6; i++)
+        {
+            if (item.itemList[i] != " ")
+            {
+                cout << item.itemList[i];
+            }
+        }
+
+        cout << "\n입력 : "; cin >> input;
+    }
+}
+
